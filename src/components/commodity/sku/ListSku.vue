b471dad9b51c9bac1851e026e7956d12cebc018d
王胜 <1689841661@qq.com>
2023-05-03 17:57:38 +0800

    改造商品列表查询功能，并更新elementUI到最新版本。

--- src/components/commodity/sku/ListSku.vue
+++ src/components/commodity/sku/ListSku.vue
@@ -33,13 +33,85 @@
 		<!-- <div class="a1">
 			<div class="abc1">wew</div>
 		</div> -->
+		<ul :loading="loading" class="infinite-list" style="overflow:auto;list-style: none;">
+			<li :key="index" v-for="(item, index) in skuDataList" class="infinite-list-item">
+				<div class="sku">
+					<el-card shadow="hover" :body-style="{ padding: '0px' }">
+						<el-image :src="require('@/assets/logo.png')" class="image"/>
+						<!-- <el-image :src="require('/Users/wangsheng/headPortrai/'+item.skuImageName+'.jpg')" class="image"/> -->
+						<!-- <el-image :src="item.skuImagePath" class="image"/> -->
+						<div style="padding: 14px;">
+							<div>
+								<span>{{ item.no }}</span>
+							</div>
+							<div>
+								<span>{{ item.name }}</span>
+							</div>
+							<div>
+								<div class="skuPrice">
+									<span class="priceIcon">¥</span>
+									<strong class="price">{{ item.price }}</strong>
+								</div>
+								<div class="skuNumber">{{ item.number }}</div>
+							</div>
+						</div>
+					</el-card>
+				</div>
+			</li>
+		</ul>
+		<el-empty v-if="skuDataList.length==0" description="无数据"></el-empty>
 		
-		<el-row :gutter="4">
+		<!-- <el-row :gutter="4">
+			<el-col :key="index" v-for="(item, index) in skuDataList" :xs="6" :sm="6" :md="6" :lg="6" :xl="6">
+				<div class="sku">
+					<el-card shadow="hover" :body-style="{ padding: '0px' }">
+						<el-image :src="item.skuImagePath" class="image"/>
+						<div style="padding: 14px;">
+							<div>
+								<span>{{ item.no }}</span>
+							</div>
+							<div>
+								<span>{{ item.name }}</span>
+							</div>
+							<div>
+								<div class="skuPrice">
+									<span class="priceIcon">¥</span>
+									<strong class="price">{{ item.price }}</strong>
+								</div>
+								<div class="skuNumber">{{ item.number }}</div>
+							</div>
+						</div>
+					</el-card>
+				</div>
+			</el-col>
+		</el-row> -->
+		<!-- <el-row :gutter="4">
 			<el-col :xs="6" :sm="6" :md="6" :lg="6" :xl="6">
 				<div class="sku">
 					<el-card shadow="hover" :body-style="{ padding: '0px' }">
-						<!-- <el-image :src="require('@/assets/logo.png')" class="image"/> -->
 						<el-image :src="require('/Users/wangsheng/headPortrai/311CDA7EC75E4893A8AD0F1930B93975.jpg')" class="image"/>
+						<div style="padding: 14px;">
+							<div>
+								<span>商品编号</span>
+							</div>
+							<div>
+								<span>名称</span>
+							</div>
+							<div>
+								<div class="skuPrice">
+									<span class="priceIcon">¥</span>
+									<strong class="price">69.90</strong>
+								</div>
+								<div class="skuNumber">数量</div>
+							</div>
+						</div>
+					</el-card>
+				</div>
+			</el-col>
+			<el-col :xs="6" :sm="6" :md="6" :lg="6" :xl="6">
+				<div class="sku">
+					<el-card shadow="hover" :body-style="{ padding: '0px' }">
+						<el-image :src="require('@/assets/logo.png')" class="image"/>
 						<div style="padding: 14px;">
 							<div>
 								<span>商品编号</span>
@@ -80,145 +152,10 @@
 					</el-card>
 				</div>
 			</el-col>
-			<el-col :xs="6" :sm="6" :md="6" :lg="6" :xl="6">
-				<div class="sku">
-					<el-card shadow="hover" :body-style="{ padding: '0px' }">
-						<el-image :src="require('/Users/wangsheng/headPortrai/311CDA7EC75E4893A8AD0F1930B93975.jpg')" class="image"/>
-						<div style="padding: 14px;">
-							<div>
-								<span>商品编号</span>
-							</div>
-							<div>
-								<span>名称</span>
-							</div>
-							<div>
-								<div class="skuPrice">
-									<span class="priceIcon">¥</span>
-									<strong class="price">69.90</strong>
-								</div>
-								<div class="skuNumber">数量</div>
-							</div>
-						</div>
-					</el-card>
-				</div>
-			</el-col>
-			<el-col :xs="6" :sm="6" :md="6" :lg="6" :xl="6">
-				<div class="sku">
-					<el-card shadow="hover" :body-style="{ padding: '0px' }">
-						<el-image :src="require('/Users/wangsheng/headPortrai/311CDA7EC75E4893A8AD0F1930B93975.jpg')" class="image"/>
-						<div style="padding: 14px;">
-							<div>
-								<span>商品编号</span>
-							</div>
-							<div>
-								<span>名称</span>
-							</div>
-							<div>
-								<div class="skuPrice">
-									<span class="priceIcon">¥</span>
-									<strong class="price">69.90</strong>
-								</div>
-								<div class="skuNumber">数量</div>
-							</div>
-						</div>
-					</el-card>
-				</div>
-			</el-col>
-		</el-row>
-		<el-row :gutter="4">
-			<el-col :xs="6" :sm="6" :md="6" :lg="6" :xl="6">
-				<div class="sku">
-					<el-card shadow="hover" :body-style="{ padding: '0px' }">
-						<!-- <el-image :src="require('@/assets/logo.png')" class="image"/> -->
-						<el-image :src="require('/Users/wangsheng/headPortrai/311CDA7EC75E4893A8AD0F1930B93975.jpg')" class="image"/>
-						<div style="padding: 14px;">
-							<div>
-								<span>商品编号</span>
-							</div>
-							<div>
-								<span>名称</span>
-							</div>
-							<div>
-								<div class="skuPrice">
-									<span class="priceIcon">¥</span>
-									<strong class="price">69.90</strong>
-								</div>
-								<div class="skuNumber">数量</div>
-							</div>
-						</div>
-					</el-card>
-				</div>
-			</el-col>
-			<el-col :xs="6" :sm="6" :md="6" :lg="6" :xl="6">
-				<div class="sku">
-					<el-card shadow="hover" :body-style="{ padding: '0px' }">
-						<el-image :src="require('/Users/wangsheng/headPortrai/311CDA7EC75E4893A8AD0F1930B93975.jpg')" class="image"/>
-						<div style="padding: 14px;">
-							<div>
-								<span>商品编号</span>
-							</div>
-							<div>
-								<span>名称</span>
-							</div>
-							<div>
-								<div class="skuPrice">
-									<span class="priceIcon">¥</span>
-									<strong class="price">69.90</strong>
-								</div>
-								<div class="skuNumber">数量</div>
-							</div>
-						</div>
-					</el-card>
-				</div>
-			</el-col>
-			<el-col :xs="6" :sm="6" :md="6" :lg="6" :xl="6">
-				<div class="sku">
-					<el-card shadow="hover" :body-style="{ padding: '0px' }">
-						<el-image :src="require('/Users/wangsheng/headPortrai/311CDA7EC75E4893A8AD0F1930B93975.jpg')" class="image"/>
-						<div style="padding: 14px;">
-							<div>
-								<span>商品编号</span>
-							</div>
-							<div>
-								<span>名称</span>
-							</div>
-							<div>
-								<div class="skuPrice">
-									<span class="priceIcon">¥</span>
-									<strong class="price">69.90</strong>
-								</div>
-								<div class="skuNumber">数量</div>
-							</div>
-						</div>
-					</el-card>
-				</div>
-			</el-col>
-			<el-col :xs="6" :sm="6" :md="6" :lg="6" :xl="6">
-				<div class="sku">
-					<el-card shadow="hover" :body-style="{ padding: '0px' }">
-						<el-image :src="require('/Users/wangsheng/headPortrai/311CDA7EC75E4893A8AD0F1930B93975.jpg')" class="image"/>
-						<div style="padding: 14px;">
-							<div>
-								<span>商品编号</span>
-							</div>
-							<div>
-								<span>名称</span>
-							</div>
-							<div>
-								<div class="skuPrice">
-									<span class="priceIcon">¥</span>
-									<strong class="price">69.90</strong>
-								</div>
-								<div class="skuNumber">数量</div>
-							</div>
-						</div>
-					</el-card>
-				</div>
-			</el-col>
-		</el-row>
+		</el-row> -->
 		<!-- 商品列表展示区域 end -->
 		<!-- 分页组件 start -->
-		<!-- <el-pagination
+		<el-pagination
 		        @size-change="handleSizeChange"
 		        @current-change="handleCurrentChange"
 		        :current-page.sync="page.currentPage"
@@ -226,27 +163,14 @@
 		        :page-size="page.pageSize"
 		        layout="total, sizes, prev, pager, next, jumper"
 		        :total="page.totalSize">
-		</el-pagination> -->
+		</el-pagination>
 		<!-- 分页组件 end -->
-		
-		<!-- <base-table :uri="uri"
-		            ref="tableSku"
-		            :operationButtonList="operationButtonList"
-		            :tableColumnList="tableColumnList"
-		            :searchData="searchData"
-		            :searchForm="searchForm"
-		            :formSize="formSize"
-					searchFormRef="searchSkuForm"
-					:multiSelect="false"
-		            :searchHandle="searchHandle" 
-					@searchDelTagOptions="searchDelTagOptions"></base-table> -->
 	</div>
 </template>
 
 <script>
 	import BreadCrumbs from "@/components/common/BreadCrumbs";
 	import BaseForm from "@/components/common/BaseForm";
-	// import BaseTable from "@/components/common/BaseTable";
 	
 	import * as SystemConstant from '@/components/constant/systemConstant';
 	import * as CommInterface from '@/components/utils/commInterface';
@@ -260,43 +184,38 @@
 	export default {
 		name: "Specification",
 		components: {
-			// "base-table": BaseTable,
 			"bread-crumbs": BreadCrumbs,
 			"base-form": BaseForm,
 		},
 		data() {
 			return {
+				loading: true,/* 加载中 */
+				skuDataList: [],
 				testImage: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-				page:{
-					currentPage: 1,
-					pageSizeArr: 1,
-					pageSize: 1,
-					totalSize: 1,
+				page:{// 分页参数
+				    currentPage: 1,// 当前页
+				    pageSizeArr:[5, 10, 20, 30, 40],// 可选每页显示数据条数
+				    pageSize: 5,// 当前每页显示条数
+				    totalSize: 1// 总页数
 				},
 				uri: SystemConstant.consSkuManage.FIND_PAGE,
 				searchData:{
-					id: "",// 产品主键
-					code: "",// 产品编码
+					no: "",// 产品编号
 					name: "",// 产品名称
-					delTag: businessConstant.NO,// 删除标记
-					typeId: "",// 货品类型
-					brandId: "",// 货品品牌
+					spuNo: "",// 货品编号
+					spuName: "",// 货品名称
+					spuTypeCode: "",// 货品类型代码
+					brandCode: "",// 货品品牌代码
+					brandName: "",/* 品牌名称 */
+					delTag: businessConstant.NO,/* 删除标记 */
 				},
 				searchForm:{
-					id: {
+					no: {
 						type: "Input",
-						label: "货品ID",
-						prop: "id",
+						label: "产品编号",
+						prop: "no",
 						width: "180px",
-						placeholder: "请输入货品ID...",
-						size:""
-					},
-					code: {
-						type: "Input",
-						label: "货品CODE",
-						prop: "code",
-						width: "180px",
-						placeholder: "请输入货品CODE...",
+						placeholder: "请输入产品编号...",
 						size:""
 					},
 					name: {
@@ -307,25 +226,49 @@
 						placeholder: "请输入规格名称...",
 						size:""
 					},
-					typeId: {
+					spuNo: {
+						type: "Input",
+						label: "货品编号",
+						prop: "spuNo",
+						width: "180px",
+						placeholder: "请输入货品编号...",
+						size:""
+					},
+					spuName: {
+						type: "Input",
+						label: "货品名称",
+						prop: "spuName",
+						width: "180px",
+						placeholder: "请输入货品名称...",
+						size:""
+					},
+					spuTypeCode: {
 						type: "Select",
-						label: "货品类型",
-						prop: "typeId",
+						label: "货品类型代码",
+						prop: "spuTypeCode",
 						width: "180px",
 						options: [],
 						change: function(v){
 							console.log("当前值",v);
 						}
 					},
-					brandId: {
+					brandCode: {
 						type: "Select",
-						label: "货品品牌",
-						prop: "brandId",
+						label: "货品品牌代码",
+						prop: "brandCode",
 						width: "180px",
 						options: [],
 						change: function(v){
 							console.log("当前值",v);
 						}
+					},
+					brandName: {
+						type: "Input",
+						label: "货品品牌名称",
+						prop: "brandName",
+						width: "180px",
+						placeholder: "请输入货品品牌名称...",
+						size:""
 					},
 					delTag: {
 						type: "Select",
@@ -347,7 +290,7 @@
 						handle:()=>{
 							let _this = this;
 							_this.loading = true;
-							_this.$refs.tableSku.queryList();// 调子组件的方法
+							_this.queryList();
 						}
 					},
 					{
@@ -356,7 +299,7 @@
 						size: "",
 						handle:()=>{
 							let _this = this;
-							_this.$refs.tableSku.$refs.searchSkuForm.$refs.defaultMyForm.resetFields();
+							_this.$refs.searchSkuForm.$refs.defaultMyForm.resetFields();
 						}
 					}
 				],
@@ -402,37 +345,37 @@
 						}
 					},
 				],
-				tableColumnList:[
-					{ prop: "id", label: "ID", width: 180 },
-					{ prop: "skuCodeName", label: "产品名称", width: 180 },
-					{ prop: "price", label: "产品价格", width: 180 },
-					{ prop: "skuNumber", label: "产品数量", width: 180 },
-					{ prop: "spuCodeName", label: "所属货品", width: 180 },
-					{ prop: "spuTypeCodeName", label: "货品类型", width: 180 },
-					{ prop: "brandCodeName", label: "所属品牌", width: 180 },
-					// { prop: "speIdName", label: "产品规格", width: 180 },
-					{ prop: "delTag", label: "删除标记", width: 80 },
-					{ prop: "remark", label: "备注"},
-					// { prop: "createUser", label: "创建人", width: 120 },
-					// { prop: "createTime", label: "创建时间", width: 160, type:SystemConstant.dataType.DATE, formatDate: SystemConstant.common.FORMAT_DATE },
-					// { prop: "updateUser", label: "修改人", width: 120 },
-					// { prop: "updateTime", label: "修改时间", width: 160, type:SystemConstant.dataType.DATE, formatDate:SystemConstant.common.FORMAT_DATE }
-				],
-				selectedData: false
 			};
 		},
 		methods: {
+			queryList(){// 查询商品列表数据
+			    let _this = this;
+			    let uri = _this.uri;
+			    console.log("searchData",_this.searchData);
+			    console.log("page",_this.page);
+			    _this.$http.post(uri,{
+			        currentPage: _this.page.currentPage,
+			        pageSize: _this.page.pageSize,
+			        params: _this.searchData
+			    }).then(function (res) {
+			        _this.skuDataList = res.data.content;
+			        _this.page.totalSize = res.data.totalSize;
+			        _this.page.currentPage = res.data.currentPage;
+			        _this.loading = false;
+					
+			    });
+			},
 			handleCurrentChange(val){// 分页组件当前页发生变化触发事件
-			    let the = this;
-			    // the.loading = true;
-			    // the.currentPage = val;
-			    // the.queryList();
+			    let _this = this;
+			    _this.loading = true;
+			    _this.currentPage = val;
+			    _this.queryList();
 			},
 			handleSizeChange(val){// 分页组件每页数据条数改变触发事件
-			    let the = this;
-			    // the.loading = true;
-			    // the.page.pageSize = val;
-			    // the.queryList();
+			    let _this = this;
+			    _this.loading = true;
+			    _this.page.pageSize = val;
+			    _this.queryList();
 			},
 			searchDelTagOptions(codeType){/* 获取删除标志的数据源 */
 				let _this = this;
@@ -455,7 +398,7 @@
 				CommInterface.searchSPUTypeCode(
 					{delTag: businessConstant.NO},
 				    function (data) {
-						_this.searchForm.typeId.options = data;
+						_this.searchForm.spuTypeCode.options = data;
 				    }
 				);
 			},
@@ -464,14 +407,13 @@
 				CommInterface.searchSpuBrand(
 					{delTag: businessConstant.NO},
 				    function (data) {
-						_this.searchForm.brandId.options = data;
+						_this.searchForm.brandCode.options = data;
 				    }
 				);
 			},
 			add(){/* 新增 */
 				let _this = this;
 				CommInterface.goToPage(SystemConstant.consComponentPath.ADD_SKU, SystemConstant.consComponentName.ADD_SKU, {});
-				// _this.$refs.tableSku.toPage(SystemConstant.consComponentPath.ADD_SKU, SystemConstant.consComponentName.ADD_SKU, {});
 			},
 			update(){/* 维护 */
 				let _this = this;
@@ -495,6 +437,7 @@
 			_this.searchSpuType();
 			_this.searchSpuBrand();
 			_this.searchDelTagOptions(businessConstant.CODE_TYPE.YES_OR_NO);
+			_this.queryList();
 		}
 	}
 </script>
@@ -522,5 +465,16 @@
 	}
 	.sku:hover {
 		margin:-10px 0 0 -10px;
+	}
+	.infinite-list {
+		padding-left: 16px;
+		padding-top: 10px;
+		/* background-color: red; */
+	}
+	.infinite-list-item {
+		float: left;
+		width: 24%;
+		margin-right: 10px;
+		height: 320px;
 	}
 </style>
